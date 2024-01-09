7d2b06c7b2491505c6eebff9f563fa16878ba0c1
Khodatskaa <khodatskaalina@gmail.com>
2024-01-09 19:49:51 +0200

    Ex 1 was done

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,17 +1,17 @@
-// main.cpp
-
 #include "student.h"
 #include "aspirant.h"
 #include <iostream>
 
 int main() {
-	Student student("John", 20, 123);
-	student.display();
-	cout << endl;
+    Student student("John", 20, 123);
+    cout << "Student(";
+    student.display();
+    cout << ")" << endl;
 
-	Aspirant aspirant("Mary", 25, 456, "Computer Science");
-	aspirant.display();
-	cout << endl;
+    Aspirant aspirant("Mary", 25, 456, "Computer Science");
+    cout << "\nAspirant(";
+    aspirant.display();
+    cout << ")" << endl;
 
-	return 0;
+    return 0;
 }
